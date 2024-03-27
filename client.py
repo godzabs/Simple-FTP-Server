8015eb6ac3fa3a7b4ff3842647322afed4ba2f01
metermult <142856655+metermult@users.noreply.github.com>
2024-03-27 12:48:21 -0400

    Update client.py
    Updated ln 20 with sending bytes. Before, it was a string that was encoded. UTF16 would have broke this method. The buffer recv in the c code was also having issues when this was being sent as a string and would cause an overflow and the program would err out.

--- client.py
+++ client.py
@@ -2,26 +2,20 @@ from socket import socket
 import os
 import re
 
-
-host = input("Give host IP: ")
-port = 3990
-
 mysock = socket()
-mysock.connect((host,port))
+mysock.connect((input("Give host IP: "),3990))
 recv = mysock.recv(1024)
 
 print(f"receieved: {recv.decode()}")
 
 fileName = input("Enter the name and path of the file you want to send, ex: C:\\Users\\test.txt: \n")
-
 file_data = open(fileName, 'rb')
 data = file_data.read()
 fileNameExtracted = re.search(r'[\\/]?([^\\/]+)$',fileName)
-#print(data)
 trueFileName = fileNameExtracted.group(1)
 mysock.send(str(trueFileName).encode())
 file_size = os.path.getsize(fileName)
 print(f"[+]Sending {fileName} with a total of {file_size} bytes\n")
 mysock.send(str(file_size).encode())
-mysock.send(str(data).encode())
+mysock.send(data)
 socket.close(mysock)
